8d51115ebfc92dfdc0842598bc28e54242d4ef64
João Felipe <lopesmonteirojoaofelipe@gmail.com>
2026-07-11 13:55:22 -0300

    code cleanup

--- main.c
+++ main.c
@@ -11,31 +11,30 @@ int main(int argc, char *argv[])
 
     int fd = 0;
 
-    fd = mkdir("teste", 0100700);
-    if(fd == -1)
-    {
-        goto erro;
-    }
+    if(mkdir("teste", 0100700) == -1){goto erro;}
+    if(mkfifo("teste/control", 0000600) == -1){goto erro;}
+    if(mkfifo("teste/ok", 0000600) == -1){goto erro;}
 
+    printf("%i\n", fd);
+    fd = open("teste/lock", O_CREAT | O_RDONLY, 0006000);
+    if(fd == -1) {goto erro;}
     close(fd);
 
-    fd = mkfifo("teste/control",         0000600);
-    if(fd == -1) {goto erro;}
+    printf("%i\n", fd);
+    fd = open("teste/pid",    O_CREAT | O_RDONLY, 0000600);
+    if(fd == -1){goto erro;}
+    close(fd);
 
-    fd = mkfifo("teste/ok",              0000600);
-    if(fd == -1) {goto erro;}
 
-    fd = open("teste/lock", O_CLOEXEC | O_CREAT,   0000600);
-    if(fd == -1) {goto erro;}
+    printf("%i\n", fd);
+    fd = open("teste/stat",   O_CREAT | O_RDONLY, 0000600); 
+    if(fd == -1){goto erro;}
+    close(fd);
 
-    fd = open("teste/pid", O_CLOEXEC | O_CREAT,    0000644);
-    if(fd == -1) {goto erro;}
-    
-    fd = open("teste/stat", O_CLOEXEC | O_CREAT,   0000644);
-    if(fd == -1) {goto erro;}
-
-    fd = open("teste/status", O_CLOEXEC | O_CREAT, 0000644);
-    if(fd == -1) {goto erro;}
+    printf("%i\n", fd);
+    fd = open("teste/status", O_CREAT | O_RDONLY, 0000600);
+    if(fd == -1){goto erro;}
+    close(fd);
 
     
 
