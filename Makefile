0275f7cb673448f18de067bf7bca267bac0da5cb
João Felipe <lopesmonteirojoaofelipe@gmail.com>
2026-07-22 00:35:30 -0300

    main refactor

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: configurit
-	./configurit
+	./configurit -p runit
 
 configurit: main.c file_creator/file_creator.c file_creator/file_creator.h
 	gcc main.c file_creator/file_creator.c -o configurit
--- main.c
+++ main.c
@@ -3,36 +3,73 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+int create_runit_service_directory(char* path){
+	
+	char temp[sizeof(path) + 11];
 
-int main(int argc, char *argv[]) {
+	strcpy(temp, path);
+
   int result;
-  result = create_file("teste", DIRECTORY, 0000700);
-  if (result) {
-    return result;
+
+  if((result = create_file(temp, DIRECTORY, 0000700))){
+		return result;
   }
-  result = create_file("teste/control", FIFO, 0000600);
-  if (result) {
-    return result;
+
+  if((result = create_file(strcat(temp, "/control"), FIFO, 0000600))){
+		return result;
   }
-  result = create_file("teste/ok", FIFO, 0000600);
-  if (result) {
-    return result;
+
+	strcpy(temp, path);
+	printf("%s\n" ,strcat(path, "/ok"));
+	strcpy(temp, path);
+  if((result = create_file(strcat(temp, "/ok"), FIFO, 0000600))){
+	return result;
   }
-  result = create_file("teste/lock", NORMAL, 0000600);
-  if (result) {
-    return result;
+
+	strcpy(temp, path);
+  if((result = create_file(strcat(temp, "/lock"), NORMAL, 0000600))){
+		return result;
   }
-  result = create_file("teste/pid", NORMAL, 0000644);
-  if (result) {
-    return result;
+
+	strcpy(temp, path);
+  if((result = create_file(strcat(temp, "/pid"), NORMAL, 0000644))){
+		return result;
   }
-  result = create_file("teste/stat", NORMAL, 0000644);
-  if (result) {
-    return result;
+
+	strcpy(temp, path);
+  if((result = create_file(strcat(temp, "/stat"), NORMAL, 0000644))){
+		return result;
   }
-  result = create_file("teste/status", NORMAL, 0000644);
-  if (result) {
-    return result;
+
+	strcpy(temp, path);
+  if((result = create_file(strcat(temp, "/status"), NORMAL, 0000644))){
+		return result;
   }
+
   return 0;
 }
+
+int main(int argc, char *argv[]) {
+	if(argc == 1){
+		printf("usage: configurit [-p <path to directory>]");
+		return -1;
+  }
+
+	int opt;
+
+	while((opt = getopt(argc, argv, "p:")) != -1){
+		switch(opt){
+			case 'p':
+				printf("Result :%i\n", create_runit_service_directory(optarg));
+				break;
+			case '?':
+				printf("Invalid option\nusage: configurit [-p <path to directory>]");
+		}
+	}
+
+	return 0;
+}
+
+
